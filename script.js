33c3510ec86e59efbf9b976edb8833ec359a0ddd
SCxoneZ <89004475+SCxoneZ@users.noreply.github.com>
2021-12-28 17:14:14 +0700

    update script.js

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ const container = document.querySelector('.container-todo');
 const input = document.querySelector('.input-todo');
 const plusBtn = document.querySelector('.btn-todo');
 
-const todos = document.querySelectorAll('.todo');
-const text = document.querySelectorAll('.text');
-const btnCheck = document.querySelectorAll('.btn-done');
-const btnDelete = document.querySelectorAll('.btn-delete');
+let todos = document.querySelectorAll('.todo');
+let text = document.querySelectorAll('.text');
+let btnCheck = document.querySelectorAll('.btn-done');
+let btnDelete = document.querySelectorAll('.btn-delete');
+
+load();
+
+plusBtn.addEventListener('click', () => {
+const container = document.querySelector('.container-todo');
+const input = document.querySelector('.input-todo');
+const plusBtn = document.querySelector('.btn-todo');
+
+let todos = document.querySelectorAll('.todo');
+let text = document.querySelectorAll('.text');
+let btnCheck = document.querySelectorAll('.btn-done');
+let btnDelete = document.querySelectorAll('.btn-delete');
+
+load();
 
 plusBtn.addEventListener('click', () => {
   
@@ -16,8 +30,18 @@ plusBtn.addEventListener('click', () => {
   
 });
 
-function checking() {
-  
+function load() {
+  if(localStorage.getItem('todo_storage')){
+    const parsed = JSON.parse(localStorage.getItem('todo_storage'));
+    for(const i in parsed){
+      const newTodo = makeTodo(parsed[i].content);
+      container.appendChild(newTodo);
+      if(parsed[i].isDone){
+        newTodo.querySelector('.text').classList.add('underline');
+      }
+    }
+    recheck();
+  }
 }
 
 function makeTodo(text){
@@ -44,74 +68,135 @@ function makeTodo(text){
 }
 
 function recheck(){
-  const todos = document.querySelectorAll('.todo');
-  const text = document.querySelectorAll('.text');
-  const btnCheck = document.querySelectorAll('.btn-done');
-  const btnDelete = document.querySelectorAll('.btn-delete');
+  reselect();
   
   btnCheck.forEach((btn) => {
     btn.addEventListener('click', (e) => {
       const theText = e.target.parentElement.parentElement.querySelector('.text');
       theText.classList.toggle('underline');
       e.target.classList.toggle('change-color');
+      save();
+  
+
+  btnDelete.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      e.target.parentElement.parentElement.remove();
+      reselect();
+      save();
+    });
+  });
+  
+  save();
+  
+}
+
+function save(){
+  let strg = {};
+  todos.forEach((el, i) => {
+    const content = el.querySelector('.text').textContent;
+    strg[`todo${i}`] = {};
+    strg[`todo${i}`].content = content;
+    strg[`todo${i}`].isDone = (el.querySelector('.text').classList.contains('underline')) ? true:false;
+  });
+  //console.log(strg);
+  localStorage.setItem('todo_storage', JSON.stringify(strg));
+}
+
+function reselect(){
+  todos = document.querySelectorAll('.todo');
+  text = document.querySelectorAll('.text');
+  btnCheck = document.querySelectorAll('.btn-done');
+  btnDelete = document.querySelectorAll('.btn-delete');
+}
+
+
+
+  if(input.value !== ''){
+    container.appendChild(makeTodo(input.value));
+    input.value = '';
+    recheck();
+  }
+  
+});
+
+function load() {
+  if(localStorage.getItem('todo_storage')){
+    const parsed = JSON.parse(localStorage.getItem('todo_storage'));
+    for(const i in parsed){
+      const newTodo = makeTodo(parsed[i].content);
+      container.appendChild(newTodo);
+      if(parsed[i].isDone){
+        newTodo.querySelector('.text').classList.add('underline');
+      }
+    }
+    recheck();
+  }
+}
+
+function makeTodo(text){
+  
+  const newTodo = document.createElement('div');
+  const newText = document.createElement('span');
+  const newPlace = document.createElement('div');
+  const newDone = document.createElement('div');
+  const newDelete = document.createElement('div');
+  
+  newTodo.classList.add('todo');
+  newText.classList.add('text');
+  newText.textContent = text;
+  newPlace.classList.add('btn-place');
+  newDone.classList.add('btn-done');
+  newDelete.classList.add('btn-delete');
+  newPlace.appendChild(newDone);
+  newPlace.appendChild(newDelete);
+  
+  newTodo.appendChild(newText);
+  newTodo.appendChild(newPlace);
+  return newTodo;
+  
+}
+
+function recheck(){
+  reselect();
+  
+  btnCheck.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const theText = e.target.parentElement.parentElement.querySelector('.text');
+      theText.classList.toggle('underline');
+      e.target.classList.toggle('change-color');
+      save();
     });
   });
 
   btnDelete.forEach((btn) => {
     btn.addEventListener('click', (e) => {
       e.target.parentElement.parentElement.remove();
+      reselect();
+      save();
     });
   });
   
-  if(text.length >= 0){
-    
-    let todos = [];
-    
-    for(let i = 0; i < text.length; i++){
-      todos.push(text[i].textContent);
-    }
-    
-    console.log(todos);
-    
-    let json = `{${(function(){
-      let jsonText = '';
-      for(let i = 0; i < todos.length; i++){
-        jsonText += `"todo${i}": "${todos[i]}",`;
-      }
-      return jsonText;
-      
-    })()}}`;
-    
-    
-    json = json.replace('",}', '"}');
-    localStorage.setItem('todo_storage', json);
-    //return JSON.parse(json);
-    
-  }
+  save();
   
 }
 
-/*function getRandHex(){
-  const alphabets = 'ABCDEF';
-  let hexCode = '#';
-  
-  for(let i = 0; i < 6; i++){
-    
-    let ToF;
-    const random1 = Math.random();
-    
-    if(random1 < 0.5){
-      ToF = true;
-    }else{
-      ToF = false;
-    }
-    
-    if(!ToF){
-      hexCode += alphabets[Math.floor(Math.random()*alphabets.length)]
-    }else{
-      hexCode += Math.floor(Math.random()*6);
-    }
-    
-  }
-  return hexCode;
-}*/
+function save(){
+  let strg = {};
+  todos.forEach((el, i) => {
+    const content = el.querySelector('.text').textContent;
+    strg[`todo${i}`] = {};
+    strg[`todo${i}`].content = content;
+    strg[`todo${i}`].isDone = (el.querySelector('.text').classList.contains('underline')) ? true:false;
+  });
+  //console.log(strg);
+  localStorage.setItem('todo_storage', JSON.stringify(strg));
+}
+
+function reselect(){
+  todos = document.querySelectorAll('.todo');
+  text = document.querySelectorAll('.text');
+  btnCheck = document.querySelectorAll('.btn-done');
+  btnDelete = document.querySelectorAll('.btn-delete');
+}
+
+
